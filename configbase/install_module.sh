3a37c3c2f5489fac2aa1cde2708393a27afbad6b
Nathan Azaria <nathanajah@gmail.com>
2021-05-10 21:12:02 -0700

    Overwrite symlinks for module installation

--- configbase/install_module.sh
+++ configbase/install_module.sh
@@ -9,13 +9,13 @@ mkdir -p $NVIM_BASE/plugins
 mkdir -p $NVIM_BASE/config
 
 # Install vim
-ln -s $SOURCE/vim/plugins $NVIM_BASE/plugins/$INSTALL_AS
-ln -s $SOURCE/vim/config $NVIM_BASE/config/$INSTALL_AS
+ln -sfn "$SOURCE/vim/plugins" "$NVIM_BASE/plugins/$INSTALL_AS"
+ln -sfn "$SOURCE/vim/config" "$NVIM_BASE/config/$INSTALL_AS"
 
 
 ZSH_BASE=~/.config/zsh/
 mkdir -p $ZSH_BASE/modules
 
 # Install zsh config
-ln -s $SOURCE/zsh $ZSH_BASE/modules/$INSTALL_AS
+ln -sfn "$SOURCE/zsh" "$ZSH_BASE/modules/$INSTALL_AS"
 
