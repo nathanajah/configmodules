d61b8057f6d131bfa00709370014e29137a7b8cb
Nathan Azaria <nathanajah@gmail.com>
2022-11-11 21:24:10 -0800

    fix init.lua

--- modules/base/vim/lua/config/init.lua
+++ modules/base/vim/lua/config/init.lua
@@ -2,6 +2,6 @@ mod, file = ...
 require(mod .. '.base')
 require(mod .. '.ctrlspace')
 require(mod .. '.localvimrc')
-require(mod .. '.nerdtree')
+require(mod .. '.nvim-tree')
 require(mod .. '.promptline')
 require(mod .. '.telescope')
