70504f2dd3d032a485cd1e6a4b99922dbee51e4c
Nathan Azaria <nathanajah@gmail.com>
2022-11-12 06:32:07 -0800

    Don't clean init.lua in config / plugins file

--- scripts/clean_modules.sh
+++ scripts/clean_modules.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
+shopt -s extglob
 
 NVIM_BASE=~/.config/nvim/
 rm -rf $NVIM_BASE/plugins
 rm -rf $NVIM_BASE/config
-rm -rf $NVIM_BASE/lua/config
-rm -rf $NVIM_BASE/lua/plugins
+rm -rf $NVIM_BASE/lua/config/!(init.lua)
+rm -rf $NVIM_BASE/lua/plugins/!(init.lua)
 
 ZSH_BASE=~/.config/zsh/
 rm -rf $ZSH_BASE/modules
