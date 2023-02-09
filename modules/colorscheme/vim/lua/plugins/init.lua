35c51e2366430117b364161b48a1d7ba6b1df10c
Nathan Azaria <nathanajah@gmail.com>
2023-02-09 09:19:27 -0800

    Add ginit.vim for nvim-qt on windows

--- modules/colorscheme/vim/lua/plugins/init.lua
+++ modules/colorscheme/vim/lua/plugins/init.lua
@@ -2,3 +2,4 @@ local packer = require('packer')
 local use = packer.use
 
 use('altercation/vim-colors-solarized')
+use('JulioJu/neovim-qt-colors-solarized-truecolor-only')
--- scripts/install_base.ps1
+++ scripts/install_base.ps1
@@ -7,3 +7,4 @@ New-Item -path ~\AppData\Local\nvim\lua\plugins -itemtype directory
 New-Item -path ~\AppData\Local\nvim\init.lua -target $RootDir\configbase\vim\init.lua -itemtype SymbolicLink
 New-Item -path ~\AppData\Local\nvim\lua\plugins\init.lua -target $RootDir\configbase\vim\lua\plugins\init.lua -itemtype SymbolicLink
 New-Item -path ~\AppData\Local\nvim\lua\config\init.lua -target $RootDir\configbase\vim\lua\config\init.lua -itemtype SymbolicLink
+New-Item -path ~\AppData\Local\nvim\ginit.vim -target $RootDir\configbase\vim\ginit.vim -itemtype SymbolicLink
