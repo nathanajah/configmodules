0742d624b452edb2c750a9e128d5486d79585a2f
Nathan Azaria <nathanajah@gmail.com>
2023-02-09 09:14:57 -0800

    Remove COQ from windows

--- modules/dev/vim/lua/plugins/init.lua
+++ modules/dev/vim/lua/plugins/init.lua
@@ -1,10 +1,15 @@
 local packer = require('packer')
 local use = packer.use
+local sysname = vim.loop.os_uname().sysname
 
 use('neovim/nvim-lspconfig')
 use('tpope/vim-fugitive')
 use('ludovicchabant/vim-lawrencium')
 use('airblade/vim-gitgutter')
-use('ms-jpq/coq_nvim')
 use('folke/trouble.nvim')
-use('ms-jpq/coq.artifacts')
+
+-- Don't install COQ if on Windows due to virtualenv not being setup yet
+if not sysname:find 'Windows' then
+  use('ms-jpq/coq_nvim')
+  use('ms-jpq/coq.artifacts')
+end
