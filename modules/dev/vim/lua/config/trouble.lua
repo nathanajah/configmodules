ab5618f65be42a782972c3a4e96ea343890ef17a
nathanajah <nathanajah@gmail.com>
2024-10-12 12:13:05 -0700

    fix

--- modules/dev/vim/lua/config/trouble.lua
+++ modules/dev/vim/lua/config/trouble.lua
@@ -1,10 +1,17 @@
-require("trouble").setup {}
-vim.keymap.set("n", "<F8>", "<cmd>Trouble symbols toggle auto_preview=false<CR>")
-vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle auto_preview=false<CR>")
-vim.keymap.set("n", "<leader>xl", "<cmd>Trouble lsp toggle auto_preview=false<CR>")
-vim.keymap.set("n", "<leader>xd", "<cmd>Trouble lsp_definitions toggle auto_preview=false<CR>")
-vim.keymap.set("n", "<leader>xD", "<cmd>Trouble lsp_declarations toggle auto_preview=false<CR>")
-vim.keymap.set("n", "<leader>xi", "<cmd>Trouble lsp_implementations toggle auto_preview=false<CR>")
-vim.keymap.set("n", "<leader>xr", "<cmd>Trouble lsp_references toggle auto_preview=false<CR>")
-vim.keymap.set("n", "<leader>xI", "<cmd>Trouble lsp_incoming_calls toggle auto_preview=false<CR>")
-vim.keymap.set("n", "<leader>xO", "<cmd>Trouble lsp_outgoing_calls toggle auto_preview=false<CR>")
+require("trouble").setup {
+  auto_preview = false,
+  win = {
+    size = {
+      width= 0.2,
+    }
+  }
+}
+vim.keymap.set("n", "<F8>", "<cmd>Trouble symbols toggle<CR>")
+vim.keymap.set("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<CR>")
+vim.keymap.set("n", "<leader>xl", "<cmd>Trouble lsp toggle win.position=right<CR>")
+vim.keymap.set("n", "<leader>xd", "<cmd>Trouble lsp_definitions toggle<CR>")
+vim.keymap.set("n", "<leader>xD", "<cmd>Trouble lsp_declarations toggle<CR>")
+vim.keymap.set("n", "<leader>xi", "<cmd>Trouble lsp_implementations toggle<CR>")
+vim.keymap.set("n", "<leader>xr", "<cmd>Trouble lsp_references toggle<CR>")
+vim.keymap.set("n", "<leader>xI", "<cmd>Trouble lsp_incoming_calls toggle<CR>")
+vim.keymap.set("n", "<leader>xO", "<cmd>Trouble lsp_outgoing_calls toggle<CR>")
